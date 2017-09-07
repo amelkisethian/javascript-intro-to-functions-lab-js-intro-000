954452527e1356605e57088154201f1f03fbe5b2
amelkisethian <angela.melkisethian@gmail.com>
2017-09-07 05:35:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string.toLowerCase) {
+  if (string.toLowerCase() === string.toLowerCase()) {
     return "I can\'t hear you!"
-  } else if (string.toUpperCase() === string.toUpperCase) {
+  } else if (string.toUpperCase() === string.toUpperCase()) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
